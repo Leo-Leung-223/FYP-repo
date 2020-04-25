2a06570cfff445be105956c8fc703528b8432008
EC2 Default User <ec2-user@ip-172-31-87-3.ec2.internal>
2020-04-25 07:36:48 +0000

    amplify commad

---
+++ amplify_init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# get access key through the system manager
+access_key_id=$(aws ssm get-parameters --names '/AmplifyCICD/AccessKeyID' --query Parameters[].Value --output text)
+secret_access_key=$(aws ssm get-parameters --names '/AmplifyCICD/SecretAccessKey' --query Parameters[].Value --output text)
+
+
+
+# AWS Profile setting
+aws configure set aws_access_key_id $access_key_id
+aws configure set aws_secret_access_key $secret_access_key
+aws configure set default.region us-east-1
+
+#Amplify setting
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":true,\
+\"profileName\":\"default\"\
+}"
+
+AMPLIFY="{\
+\"projectName\":\"sample_app\",\
+\"envName\":\"prod\",\
+\"defaultEditor\":\"code\"\,
+
+}"
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+# Amplify init
+amplify init \
+--amplify $AMPLIFY \
+--providers $PROVIDERS \
+--yes
