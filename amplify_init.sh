bb12646fd1c7783bc8b8dc23609f34da38ba5d9a
EC2 Default User <ec2-user@ip-172-31-87-3.ec2.internal>
2020-04-25 08:51:39 +0000

    change bin bash

--- amplify_init.sh
+++ amplify_init.sh
@@ -1,4 +1,6 @@
-#/bin/bash
+#!/bin/bash
+set -e
+IFS='|'
 # get access key through the system manager
 access_key_id=$(aws ssm get-parameters --names '/AmplifyCICD/AccessKeyID' --query Parameters[].Value --output text)
 secret_access_key=$(aws ssm get-parameters --names '/AmplifyCICD/SecretAccessKey' --query Parameters[].Value --output text)
