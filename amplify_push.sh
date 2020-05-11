4e207270a6b6647187ec816d415854708e840f13
180133517 <48379051+180133517@users.noreply.github.com>
2020-05-11 11:44:10 +0800

    revert to sumerian json

--- amplify_push.sh
+++ amplify_push.sh
@@ -1,113 +1,49 @@
 #!/bin/bash
 set -e
 IFS='|'
+# get access key through the system manager
+access_key_id=$(aws ssm get-parameters --names '/AmplifyCICD/AccessKeyID' --query Parameters[].Value --output text)
+secret_access_key=$(aws ssm get-parameters --names '/AmplifyCICD/SecretAccessKey' --query Parameters[].Value --output text)
 
-help_output () {
-    echo "usage: amplify-push <--environment|-e <name>> <--simple|-s>"
-    echo "  --environment  The name of the Amplify environment to use"
-    echo "  --simple  Optional simple flag auto-includes stack info from env cache"
-    exit 1
-}
 
-init_env () {
-    ENV=$1
-    AMPLIFY=$2
-    PROVIDERS=$3
-    CODEGEN=$4
-    AWSCONFIG=$5
 
-    echo "# Start initializing Amplify environment: ${ENV}"
-    if [[ -z ${STACKINFO} ]];
-    then
-        echo "# Initializing new Amplify environment: ${ENV} (amplify init)"
-        amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes;
-        echo "# Environment ${ENV} details:"
-        amplify env get --name ${ENV}
-    else
-        echo "STACKINFO="${STACKINFO}
-        echo "# Importing Amplify environment: ${ENV} (amplify env import)"
-        amplify env import --name ${ENV} --config "${STACKINFO}" --awsInfo ${AWSCONFIG} --yes;
-        echo "# Initializing existing Amplify environment: ${ENV} (amplify init)"
-        amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes;
-        echo "# Environment ${ENV} details:"
-        amplify env get --name ${ENV}
-    fi
-    echo "# Done initializing Amplify environment: ${ENV}"
-}
+# AWS Profile setting
+aws configure set aws_access_key_id $access_key_id
+aws configure set aws_secret_access_key $secret_access_key
+aws configure set default.region us-east-1
 
-ENV=""
-IS_SIMPLE=false
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-    do
-    key="$1"
-    case ${key} in
-        -e|--environment)
-        ENV=$2
-        shift
-        ;;
-        -r|--region)
-        REGION=$2
-        shift
-        ;;
-        -s|--simple)
-        IS_SIMPLE=true
-        shift
-        ;;
-        *)
-        POSITIONAL+=("$1")
-        shift
-        ;;
-    esac
-done
-set -- "${POSITIONAL[@]}"
-
-# if no provided environment name, use default env variable, then user override
-if [[ ${ENV} = "" ]];
-then
-    ENV=${AWS_BRANCH}
-fi
-
-if [[ ${USER_BRANCH} != "" ]];
-then
-    ENV=${USER_BRANCH}
-fi
-
-# strip slashes, limit to 10 chars
-ENV=$(echo ${ENV} | sed 's;\\;;g' | sed 's;\/;;g' | cut -c -10)
-
-# Check valid environment name
-if [[ -z ${ENV} || "${ENV}" =~ [^a-zA-Z0-9\-]+ ]] ; then help_output ; fi
-
-AWSCONFIG="{\
+REACTCONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"build\",\
+\"BuildCommand\":\"npm run-script build\",\
+\"StartCommand\":\"npm run-script start\"\
+}"
+AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":true,\
 \"profileName\":\"default\",\
-\"AmplifyAppId\":\"${AWS_APP_ID}\"\
-}"
-AMPLIFY="{\
-\"envName\":\"${ENV}\",\
-\"appId\":\"${AWS_APP_ID}\"\
-}"
-PROVIDERS="{\
-\"awscloudformation\":${AWSCONFIG}\
-}"
-CODEGEN="{\
-\"generateCode\":false,\
-\"generateDocs\":false\
+\"region\":\"us-east-1\"\
 }"
 
-# Handle old or new config file based on simple flag
-if [[ ${IS_SIMPLE} ]];
-then
-    echo "# Getting Amplify CLI Cloud-Formation stack info from environment cache"
-    export STACKINFO="$(envCache --get stackInfo)"
-    init_env ${ENV} ${AMPLIFY} ${PROVIDERS} ${CODEGEN} ${AWSCONFIG}
-    echo "# Store Amplify CLI Cloud-Formation stack info in environment cache"
-    STACKINFO="$(amplify env get --json --name ${ENV})"
-    envCache --set stackInfo ${STACKINFO}
-    echo "STACKINFO="${STACKINFO}
-else
-    # old config file, above steps performed outside of this script
-    init_env ${ENV} ${AMPLIFY} ${PROVIDERS} ${CODEGEN} ${AWSCONFIG}
-fi
+AMPLIFY="{\
+\"projectName\":\"webapp\",\
+\"envName\":\"dev\",\
+\"defaultEditor\":\"code\"\
+}"
+
+FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"react\",\
+\"config\":$REACTCONFIG\
+}"
+
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+# Amplify init
+amplify init \
+--amplify $AMPLIFY \
+--frontend $FRONTEND \
+--providers $PROVIDERS \
+--yes
